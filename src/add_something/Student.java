9f601553fe755d50d808583798345bacf1e654c2
Michael Allan <huangzq30@163.com>
2017-01-11 09:55:10 +0800

    student

---
+++ src/add_something/Student.java
@@ -0,0 +1,40 @@
+package add_something;
+
+/**
+ * Created by hzq on 17-1-11.
+ */
+public class Student {
+    /**
+     * 年龄
+     */
+    private int age;
+    /**
+     * 学员ID
+     */
+    private int id;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0){
+            //处理代码
+        }
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        //校验id是否合法的代码
+        this.id = id;
+    }
+//构造方法，初始化age，id
+    public Student(int age, int id) {
+        this.age = age;
+        this.id = id;
+    }
+}
