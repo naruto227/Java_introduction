4ef584060e0f27cccfe9d40363d22934cf4b99ee
Michael Allan <huangzq30@163.com>
2017-01-27 22:16:50 +0800

    1

--- src/string/StringFunc.java
+++ src/string/StringFunc.java
@@ -4,4 +4,8 @@ package string;
  * Created by Michael Allan on 2017/1/27.
  */
 public class StringFunc {
+    public static void main(String[] args) {
+        String s = "abc";
+        s = "Java语言";
+    }
 }
