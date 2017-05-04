9b5044d982b1d50c3b7997176afe25f0525f49de
Michael Allan <huangzq30@163.com>
2017-05-04 10:47:27 +0800

    验证码生成

--- src/casual_idea/MyString.java
+++ src/casual_idea/MyString.java
@@ -1,13 +1,18 @@
 package casual_idea;
 
+import java.util.Scanner;
+
 /**
  * Created by Michael Allan on 2017/5/3.
  */
 public class MyString {
+    private static int temp;
+//    private static String pw;
 
     public static void main(String[] args) {
         StringBuilder res = new StringBuilder();
-        System.out.println('a'+1);//"a"+1结果为a1,'a'+1结果为98
+        StringBuilder pw = new StringBuilder();
+        System.out.println('a' + 1);//"a"+1结果为a1,'a'+1结果为98
         //abcdefghijklmnopqrstuvwxyz
         for (char i = 'a'; i <= 'z'; i++) {
             res.append(i);
@@ -18,6 +23,22 @@ public class MyString {
         for (int i = 0; i < 10; i++) {
             res.append(i);
         }
-        System.out.println(res.append(str));
+//        System.out.println((int)(Math.random()*10));
+        res = res.append(str);
+        int length = res.length() - 1;
+        System.out.println(res);
+
+        System.out.print("请输入您想要生成几位的随机验证码：");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            temp = scanner.nextInt();
+            System.out.println(temp);
+            for (int i = 0; i < temp; i++) {
+                pw.append(res.charAt((int) (Math.random() * length)));
+            }
+            System.out.println("生成的验证码为：" + pw);
+            System.out.print("请输入您想要生成几位的随机验证码：");
+        }
     }
 }
